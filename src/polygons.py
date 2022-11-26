b6453800359207286fd190caf99ffdbfd6f528f0
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2022-11-26 21:12:50 +0100

    Create polygons.py
    Create a polygons module that contains the Triangle class, used to represent triangles.

---
+++ src/polygons.py
@@ -0,0 +1,93 @@
+from coordinate import Coordinate2D
+
+
+class Triangle:
+
+    def __init__(self, a: Coordinate2D, b: Coordinate2D,
+                 c: Coordinate2D) -> None:
+
+        self._initial_setting = False
+
+        self.a = a
+        self.b = b
+        self.c = c
+
+        self._initial_setting = True
+
+    @property
+    def a(self) -> Coordinate2D:
+        """First vertex of the triangle.
+
+        Returns:
+            Coordinate2D: First vertex of the triangle.
+        """
+
+        return self._a
+
+    @a.setter
+    def a(self, value: Coordinate2D) -> None:
+        """First vertex of the triangle.
+
+        Args:
+            value (Coordinate2D): First vertex of the triangle.
+
+        Raises:
+            TypeError: If the value is not a Coordinate2D object.
+        """
+
+        if not isinstance(value, Coordinate2D):
+            raise TypeError("a must be a Coordinate2D instance")
+
+        self._a = value
+
+    @property
+    def b(self) -> Coordinate2D:
+        """Second vertex of the triangle.
+
+        Returns:
+            Coordinate2D: Second vertex of the triangle.
+        """
+
+        return self._b
+
+    @b.setter
+    def b(self, value: Coordinate2D) -> None:
+        """Second vertex of the triangle.
+
+        Args:
+            value (Coordinate2D): Second vertex of the triangle.
+
+        Raises:
+            TypeError: If the value is not a Coordinate2D object.
+        """
+
+        if not isinstance(value, Coordinate2D):
+            raise TypeError("b must be a Coordinate2D instance")
+
+        self._b = value
+
+    @property
+    def c(self) -> Coordinate2D:
+        """Third vertex of the triangle.
+
+        Returns:
+            Coordinate2D: Third vertex of the triangle.
+        """
+
+        return self._c
+
+    @c.setter
+    def c(self, value: Coordinate2D) -> None:
+        """Third vertex of the triangle.
+
+        Args:
+            value (Coordinate2D): Third vertex of the triangle.
+
+        Raises:
+            TypeError: If the value is not a Coordinate2D object.
+        """
+
+        if not isinstance(value, Coordinate2D):
+            raise TypeError("c must be a Coordinate2D instance")
+
+        self._c = value
