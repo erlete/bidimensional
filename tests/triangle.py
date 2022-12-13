1924e36a08b4f012520d6e95254d38377e2de24b
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2022-12-13 15:12:10 +0100

    Update documentation and improve methods
    Updated docstrings and grouped `is_collinear` method tests in order to provide with a solid test case.

--- tests/triangle.py
+++ tests/triangle.py
@@ -5,38 +5,71 @@ from itertools import combinations, permutations
 
 
 class TestTriangle:
+    """Triangle tests class.
+
+    This class contains all relevant test cases for the `polygons.triangle`
+    module.
+
+    Note:
+        Tests contained here are intended to be run with the `pytest` module.
+    """
 
     DEFINITION_TOL = 1e-14
 
-    def test_generation(self):
+    def test_generation(self) -> None:
+        """Triangle generation test.
+
+        This test case checks if the triangle is correctly generated from the
+        given coordinates. It also checks if the definition order is correct
+        and each vertex is associated with the correct coordinate.
+        """
+
         triangle = Triangle(
             Coordinate(0, 0),
             Coordinate(1, 1),
             Coordinate(1, 0)
         )
 
+        # Coordinate components' checks:
+
         assert triangle.a.x == 0 and triangle.a.y == 0
         assert triangle.b.x == 1 and triangle.b.y == 1
         assert triangle.c.x == 1 and triangle.c.y == 0
 
-    def test_definition_order(self):
+    def test_definition_order(self) -> None:
+        """Triangle definition order test.
+
+        This test case checks if the definition order matters defines unique
+        triangle objects. This should not be the case, as the definition order
+        should not matter.
+        """
+
         coordinates = (
             Coordinate(0, 0),
             Coordinate(1, 1),
             Coordinate(1, 0)
         )
 
+        # Generate all possible triangle definitions:
+
         triangles = [
             Triangle(*triplet)
             for triplet in permutations(coordinates, 3)
         ]
+
+        # Check if all triangles are equal:
 
         assert all(
             pair[0] == pair[1]
             for pair in combinations(triangles, 2)
         )
 
-    def test_area(self):
+    def test_area(self) -> None:
+        """Triangle area test.
+
+        This test case checks if the triangle area is correctly calculated.
+        """
+
         triangle = Triangle(
             Coordinate(0, 0),
             Coordinate(1, 1),
@@ -45,7 +78,13 @@ class TestTriangle:
 
         assert triangle.area == 0.5
 
-    def test_perimeter(self):
+    def test_perimeter(self) -> None:
+        """Triangle perimeter test.
+
+        This test case checks if the triangle perimeter is correctly
+        calculated.
+        """
+
         triangle = Triangle(
             Coordinate(0, 0),
             Coordinate(1, 1),
@@ -54,52 +93,46 @@ class TestTriangle:
 
         assert triangle.perimeter == sqrt(2) + 2
 
-    def test_collinear_1(self):
-        triangle = Triangle(
-            Coordinate(0, 0),
-            Coordinate(1, 1),
-            Coordinate(2, 2)
+    def test_collinear_1(self) -> None:
+        """Vertex collinearity test 1.
+
+        This test case checks if the `is_collinear` method correctly detects
+        vertex alignments on three scenarios:
+            Vertical alignment (Y axis)
+            Horizontal alignment (X axis)
+            Diagonal alignment (y = ax + b form)
+        """
+
+        coordinates = (
+            (  # Vertical alignment:
+                Coordinate(0, 0),
+                Coordinate(0, 1),
+                Coordinate(0, 2)
+            ), (  # Horizontal alignment:
+                Coordinate(1, 0),
+                Coordinate(2, 0),
+                Coordinate(3, 0)
+            ), (  # Diagonal alignment:
+                Coordinate(0, 0),
+                Coordinate(1, 1),
+                Coordinate(2, 2)
+            )
         )
 
-        assert triangle.is_collinear() is True
+        # Coordinate order mixing:
 
-    def test_collinear_2(self):
-        triangle = Triangle(
-            Coordinate(0, 0),
-            Coordinate(1, 1),
-            Coordinate(2, 3)
-        )
+        for coordinate_set in coordinates:
+            for triplet in permutations(coordinate_set, 3):
+                triangle = Triangle(*triplet)
+                assert triangle.is_collinear()
 
-        assert triangle.is_collinear() is False
+    def test_eq(self) -> None:
+        """Triangle equality test.
 
-    def test_collinear_3(self):
-        triangle = Triangle(
-            Coordinate(0, 0),
-            Coordinate(0, 1),
-            Coordinate(0, -1)
-        )
+        This test case checks if the triangle equality operator correctly
+        detects equal triangles.
+        """
 
-        assert triangle.is_collinear() is True
-
-    def test_collinear_4(self):
-        triangle = Triangle(
-            Coordinate(0, 0),
-            Coordinate(1, 0),
-            Coordinate(-1, 0)
-        )
-
-        assert triangle.is_collinear() is True
-
-    def test_collinear_5(self):
-        triangle = Triangle(
-            Coordinate(0, 0),
-            Coordinate(1, 1),
-            Coordinate(2, 3)
-        )
-
-        assert triangle.is_collinear() is False
-
-    def test_eq(self):
         triangle_1 = Triangle(
             Coordinate(0, 0),
             Coordinate(1, 1),
@@ -114,7 +147,13 @@ class TestTriangle:
 
         assert triangle_1 == triangle_2
 
-    def test_neq(self):
+    def test_neq(self) -> None:
+        """Triangle inequality test.
+
+        This test case checks if the triangle inequality operator correctly
+        detects inequal triangles.
+        """
+
         triangle_1 = Triangle(
             Coordinate(0, 0),
             Coordinate(1, 1),
@@ -129,7 +168,13 @@ class TestTriangle:
 
         assert triangle_1 != triangle_2
 
-    def test_gt(self):
+    def test_gt(self) -> None:
+        """Triangle greater than test.
+
+        This test case checks if the triangle greater than operator correctly
+        detects greater triangles.
+        """
+
         triangle_1 = Triangle(
             Coordinate(0, 0),
             Coordinate(1, 1),
@@ -144,7 +189,13 @@ class TestTriangle:
 
         assert triangle_1 > triangle_2
 
-    def test_lt(self):
+    def test_lt(self) -> None:
+        """Triangle less than test.
+
+        This test case checks if the triangle less than operator correctly
+        detects lesser triangles.
+        """
+
         triangle_1 = Triangle(
             Coordinate(0, 0),
             Coordinate(1, 1),
@@ -159,7 +210,13 @@ class TestTriangle:
 
         assert triangle_1 < triangle_2
 
-    def test_ge(self):
+    def test_ge(self) -> None:
+        """Triangle greater than or equal test.
+
+        This test case checks if the triangle greater than or equal operator
+        correctly detects greater or equal triangles.
+        """
+
         triangle_1 = Triangle(
             Coordinate(0, 0),
             Coordinate(1, 1),
@@ -175,7 +232,13 @@ class TestTriangle:
         assert triangle_1 >= triangle_2
         assert triangle_1 >= triangle_1
 
-    def test_le(self):
+    def test_le(self) -> None:
+        """Triangle less than or equal test.
+
+        This test case checks if the triangle less than or equal operator
+        correctly detects lesser or equal triangles.
+        """
+
         triangle_1 = Triangle(
             Coordinate(0, 0),
             Coordinate(1, 1),
@@ -191,7 +254,13 @@ class TestTriangle:
         assert triangle_1 <= triangle_2
         assert triangle_1 <= triangle_1
 
-    def test_is_right(self):
+    def test_is_right(self) -> None:
+        """Triangle right angle test.
+
+        This test case checks if the `is_right` method correctly detects
+        right triangles.
+        """
+
         coordinates = (
             Coordinate(0, 0),
             Coordinate(1, 0),
@@ -203,7 +272,13 @@ class TestTriangle:
             for triplet in permutations(coordinates, 3)
         )
 
-    def test_is_obtuse(self):
+    def test_is_obtuse(self) -> None:
+        """Triangle obtuse angle test.
+
+        This test case checks if the `is_obtuse` method correctly detects
+        obtuse triangles.
+        """
+
         coordinates = (
             Coordinate(0, 0),
             Coordinate(1, 0),
@@ -215,7 +290,13 @@ class TestTriangle:
             for triplet in permutations(coordinates, 3)
         )
 
-    def test_is_acute(self):
+    def test_is_acute(self) -> None:
+        """Triangle acute angle test.
+
+        This test case checks if the `is_acute` method correctly detects
+        acute triangles.
+        """
+
         coordinates = (
             Coordinate(0, 0),
             Coordinate(1, 0),
@@ -227,7 +308,13 @@ class TestTriangle:
             for triplet in permutations(coordinates, 3)
         )
 
-    def test_is_equilateral(self):
+    def test_is_equilateral(self) -> None:
+        """Triangle equilateral test.
+
+        This test case checks if the `is_equilateral` method correctly detects
+        equilateral triangles.
+        """
+
         coordinates = (
             Coordinate(0, 0),
             Coordinate(1, 0),
@@ -239,7 +326,13 @@ class TestTriangle:
             for triplet in permutations(coordinates, 3)
         )
 
-    def test_is_isosceles(self):
+    def test_is_isosceles(self) -> None:
+        """Triangle isosceles test.
+
+        This test case checks if the `is_isosceles` method correctly detects
+        isosceles triangles.
+        """
+
         coordinates = (
             Coordinate(0, 0),
             Coordinate(1, 0),
@@ -251,7 +344,13 @@ class TestTriangle:
             for triplet in permutations(coordinates, 3)
         )
 
-    def test_is_scalene(self):
+    def test_is_scalene(self) -> None:
+        """Triangle scalene test.
+
+        This test case checks if the `is_scalene` method correctly detects
+        scalene triangles.
+        """
+
         coordinates = (
             Coordinate(0, 0),
             Coordinate(1, 0),
