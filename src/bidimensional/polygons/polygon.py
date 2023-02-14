41fcfb9f36e3282feb0ff269ecd3934a5445fa81
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2023-02-14 16:21:24 +0100

    Document module and add methods
    Added a new plot method for the `Polygon` class, documented it and also added a module docstring.

--- src/bidimensional/polygons/polygon.py
+++ src/bidimensional/polygons/polygon.py
@@ -1,27 +1,58 @@
+"""Container module for the Polygon class.
+
+Author:
+    Paulo Sanchez (@erlete)
+"""
+
 from __future__ import annotations
 
 from itertools import combinations_with_replacement as cr
 from math import ceil
 from string import ascii_lowercase, ascii_uppercase
 
+import matplotlib
 import matplotlib.pyplot as plt
 
 from bidimensional import Coordinate
 
 
 class Polygon:
+    """Generic polygon class.
 
-    ANNOTATIONS = {
+    This class represents a generic polygon in the bidimensional plane. It is
+    composed of a set of vertices, which are instances of the Coordinate class.
+
+    Attributes:
+        ANNOTATIONS (dict[str, str]): annotations for the vertices and sides of
+            the polygon.
+        vertices (dict[str, Coordinate]): vertices of the polygon.
+    """
+
+    ANNOTATIONS: dict[str, str] = {
         "vertices": ascii_uppercase,
         "sides": ascii_lowercase
     }
 
     def __init__(self, *vertices: Coordinate):
-        self.vertices = vertices
+        """Initialize a polygon instance.
+
+        Args:
+            *vertices (Coordinate): vertices of the polygon.
+        """
+        self.vertices = vertices  # type: ignore
 
     def _get_annotations(self, vertices: tuple[Coordinate]) -> list[str]:
+        """Compute the annotations for the vertices.
+
+        Args:
+            vertices (tuple[Coordinate]): vertices of the polygon.
+
+        Returns:
+            list[str]: annotations for the vertices.
+        """
         annotations = self.ANNOTATIONS["vertices"]
         padding = ceil(len(vertices) / len(annotations))
+
         return [
             ''.join(letters)
             for letters in sorted(
@@ -34,10 +65,26 @@ class Polygon:
 
     @property
     def vertices(self) -> dict[str, Coordinate]:
+        """Get the vertices of the polygon.
+
+        Returns:
+            dict[str, Coordinate]: vertices of the polygon.
+        """
         return self._vertices
 
     @vertices.setter
     def vertices(self, vertices: tuple[Coordinate]) -> None:
+        """Set the vertices of the polygon.
+
+        Args:
+            vertices (tuple[Coordinate]): vertices of the polygon.
+
+        Raises:
+            TypeError: if any of the vertices is not of type Coordinate.
+        """
+        if any(not isinstance(vertex, Coordinate) for vertex in vertices):
+            raise TypeError("all vertices must be of type Coordinate")
+
         self._vertices = {
             annotation: coordinate
             for annotation, coordinate in zip(
@@ -46,13 +93,30 @@ class Polygon:
             )
         }
 
-    def plot(self):
+    def plot(self, ax: matplotlib.axes.Axes = None,
+             annotate: bool = True, **kwargs) -> None:
+        """Plot the polygon.
+
+        Args:
+            ax (matplotlib.axes.Axes, optional): axes to plot on. Defaults to
+                None.
+            annotate (bool, optional): whether to annotate the vertices.
+                Defaults to True.
+            **kwargs: keyword arguments for `matplotlib.pyplot.plot`.
+        """
+        if ax is None:
+            ax = plt.gca()
+
         x = [vertex.x for vertex in self._vertices.values()]
         y = [vertex.y for vertex in self._vertices.values()]
 
-        plt.plot(x, y, 'k-')
+        ax.plot(
+            x + [x[0]],
+            y + [y[0]],
+            **kwargs
+        )
 
         for label, vertex in self._vertices.items():
-            print(f"{label = }   {vertex = }")
-            plt.plot(*vertex, 'ro')
-            plt.annotate(label, vertex)
+            vertex.plot(ax=ax)
+            if annotate:
+                ax.annotate(label, vertex, fontsize=12, fontweight="bold")
